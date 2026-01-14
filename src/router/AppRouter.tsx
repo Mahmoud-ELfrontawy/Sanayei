1aa1752e32fd4d51262e2de638abdaa7de448152
Mahmoud-ELfrontawy <123123mahmoudmohamed@gmail.com>
2026-01-14 13:11:40 +0200

    change file AppRouter.tsx in router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,11 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import ServicesPage from "../pages/Services/ServicesPage";
-<<<<<<< HEAD
 import JoinPage from "../pages/Join/JoinPage";
-=======
 import ContactPage from "../pages/Contact/ContactPage";
->>>>>>> a44273d8f935d04f0c42e4a0f8f0bd0a7124ce10
+
 
 const AppRouter: React.FC = () => {
     return (
